26209398a3b8049c4f5156c185209a2eecc2adee
rraamm8 <garam.rachel.lee@gmail.com>
2024-09-24 14:21:58 +0900

    re

--- src/App.js
+++ src/App.js
@@ -11,25 +11,26 @@ function App() {
     <div className="w-full xl:w-10/12 h-screen mx-auto
                     flex flex-col justify-center items-center">
       <header className='w-full h-20
-                        flex justify-between items-center
-                        bg-slate-200'>
-      <p className='text-2xl font-bold p-10'>
-        K-digital 8기</p>                     
-      <p className='text-4xl font-bold p-5'>
-        <TiHome />
-      </p>
-
+                         flex justify-between items-center
+                         bg-slate-200'>
+        <p className='text-2xl font-bold p-5'>
+          K-digital 8기
+        </p>
+        <p className='text-4xl font-bold p-5'>
+          <FaHome />
+        </p>
       </header>
-      <main className ='w-full grow
-                        flex flex-col justify-center items-center
-                        overflow-y-scroll'>
+      <main className='w-full grow
+                       flex flex-col justify-center items-center
+                       overflow-y-auto'>
 
-      <MyList />
+        {/* <MyDiv1 /> */}
+        <MyList />
       </main>
       <footer className='w-full h-20
-                        flex justify-center items-center
-                        bg-black text-white'>
-        <p> K-digital 8기 이가람</p>
+                         flex justify-center items-center
+                         bg-black text-white'>
+        <p>K-digital 8기 이가람</p>                  
       </footer>
     </div>
   );
