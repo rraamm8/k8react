5c3c031f6b8112b6e65a6ecf3008fd30987cf0e1
rraamm8 <garam.rachel.lee@gmail.com>
2024-09-24 17:56:57 +0900

    re

--- src/05/TailBall.js
+++ src/05/TailBall.js
@@ -2,18 +2,18 @@
 export default function TailBall({n}) {
   const ballColor = {
     'b0' : 'bg-red-800',
-    'b1' : 'bg-yellow-800'
-    'b2' : 'bg-orange-800'
-    'b3' : 'bg-green-800'
+    'b1' : 'bg-yellow-800',
+    'b2' : 'bg-orange-800',
+    'b3' : 'bg-green-800',
     'b4' : 'bg-purple-800'
   }
 
   return (
-    <div className={`"w-12 h-12 m-2
+    <div className={`w-20 h-20 m-2
                     flex justify-center items-center
                     rounded-full
                     ${ballColor['b' + Math.floor(n/10)]}
-                    font-bold text-white text-xl
+                    font-bold text-white text-2xl
                     `}>
       {n}
     </div>
